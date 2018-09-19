178f2a4e5a386ff6720b752ae7f9746eb8dfaf56
ufologist <klsforever@gmail.com>
2018-09-19 21:28:29 +0800

    实现 set/get/has/delete/clear

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
 # weapp-simple-storage
+
+[![NPM version][npm-image]][npm-url] [![changelog][changelog-image]][changelog-url] [![license][license-image]][license-url]
+
+[npm-image]: https://img.shields.io/npm/v/weapp-simple-storage.svg?style=flat-square
+[npm-url]: https://npmjs.org/package/weapp-simple-storage
+[license-image]: https://img.shields.io/badge/License-MIT-blue.svg?style=flat-square
+[license-url]: https://github.com/ufologist/weapp-simple-storage/blob/master/LICENSE
+[changelog-image]: https://img.shields.io/badge/CHANGE-LOG-blue.svg?style=flat-square
+[changelog-url]: https://github.com/ufologist/weapp-simple-storage/blob/master/CHANGELOG.md
+
 微信小程序的简单存储, 支持 TTL(time-to-live) 缓存自动过期
+
+## 安装
+
+```
+npm install weapp-simple-storage --save
+```
+
+## 使用方法
+
+```javascript
+import SimpleStorage from 'weapp-simple-storage';
+ 
+var simpleStorage = new SimpleStorage({
+    name: '_weapp_simple_storage'
+});
+simpleStorage.set('key', {
+    foo: 'bar'
+});
+simpleStorage.get('key');
+```
+
+## 配置项
+
+### name
+
+### loggerLevel
+
+## API
+
+### set
+
+### get
+
+### has
+
+### delete
+
+### clear
+
+### keys
+
+### setTtl
+
+### getTtl
+
+## Thanks
+
+* [ZaDarkSide/simpleStorage](https://github.com/ZaDarkSide/simpleStorage)
---
+++ src/weapp-simple-storage.js
@@ -0,0 +1,168 @@
+import Logger from 'simple-console-log-level';
+import extend from 'extend';
+
+/**
+ * 微信小程序的简单存储
+ * 
+ * - 支持 TTL(time-to-live) 缓存自动过期
+ * 
+ * @example
+ * import SimpleStorage from 'weapp-simple-storage';
+ * 
+ * var simpleStorage = new SimpleStorage({
+ *     name: '_weapp_simple_storage'
+ * });
+ * simpleStorage.set('key', {
+ *     foo: 'bar'
+ * });
+ * 
+ * @see https://github.com/ZaDarkSide/simpleStorage
+ */
+class WeappSimpleStorage {
+    /**
+     * 
+     * @param {object} options
+     *                 options.name {string} name 存储的名称, 默认为: _weapp_simple_storage, 后续所有的存储都挂在这个 key 值上, 会自动追加上时间戳防止 key 冲突
+     *                 options.loggerLevel {string} 日志级别, 默认为: Logger.LEVEL_WARN 
+     */
+    constructor(options) {
+        this._options = extend(true, {}, WeappSimpleStorage.defaults, options);
+
+        this._name = this._options.name;
+        this._meta = '_meta_' + this._name;
+
+        this._storage = null;
+        this._initSuccess = false;
+
+        this._initStorage();
+        this._syncStorage2Memory();
+
+        this.logger = new Logger({
+            level: this._options.loggerLevel,
+            prefix: `[${this._name}]`
+        });
+    }
+
+    _initStorage() {
+        this._storage = {
+            [this._meta]: this.getInitMeta()
+        };
+    }
+
+    _syncStorage2Memory() {
+        try {
+            var storage = wx.getStorageSync(this._name);
+
+            if (storage) {
+                this._storage = storage;
+            }
+
+            this._initSuccess = true;
+        } catch (error) {
+            this.logger.error('WeappSimpleStorage 同步缓存数据失败', error)
+        }
+    }
+
+    /**
+     * 将内存数据异步写入到缓存
+     */
+    _syncMemory2Storage(options) {
+        wx.setStorage(extend({
+            key: this._name,
+            data: this._storage
+        }, options));
+    }
+
+    /**
+     * 获取元数据, 可以扩展这里实现更多功能
+     * 
+     * @return {object} 存储的元数据
+     */
+    getInitMeta() {
+        return {
+            ttl: {}
+        };
+    }
+
+    onSet(key, newValue, oldValue) {
+        this._syncMemory2Storage({
+            success: () => {
+                this.logger.log('onSet success', key, newValue, oldValue);
+            },
+            fail: () => {
+                this.logger.warn('onSet fail', key, newValue, oldValue);
+            }
+        });
+    }
+
+    onDelete(key) {
+        this._syncMemory2Storage({
+            success: () => {
+                this.logger.log('onDelete success', key);
+            },
+            fail: () => {
+                this.logger.warn('onDelete fail', key);
+            }
+        });
+    }
+
+    onClear() {
+        this._syncMemory2Storage({
+            success: () => {
+                this.logger.log('onClear success');
+            },
+            fail: () => {
+                this.logger.warn('onClear fail');
+            }
+        });
+    }
+
+    set(key, value, options) { // simpleStorage.get
+        var oldValue = this._storage[key];
+        this._storage[key] = value;
+        this.onSet(key, value, oldValue)
+    }
+    get(key) { // simpleStorage.set
+        return this._storage[key];
+    }
+    has(key) { // simpleStorage.hasKey
+        return typeof this.get(key) != undefined;
+    }
+    /**
+     * 
+     * @return {boolean}
+     */
+    delete(key) { // simpleStorage.deleteKey
+        var result = delete this._storage[key];
+        this.onDelete(key);
+        return result;
+    }
+    clear() { // simpleStorage.flush
+        this._initStorage();
+        this.onClear();
+    }
+
+    // setTtl(key, ttl) { // simpleStorage.setTTL
+
+    // }
+    // getTtl(key) { // simpleStorage.getTTL
+
+    // }
+
+    /**
+     * 
+     * @return {Array<string>}
+     */
+    keys() { // simpleStorage.index
+        return Object.keys(this._storage).filter((key) => {
+            return key !== this._meta;
+        });
+    }
+}
+
+WeappSimpleStorage.defaults = {
+    name: '_weapp_simple_storage',
+    loggerLevel: Logger.LEVEL_WARN
+};
+
+export default WeappSimpleStorage;
